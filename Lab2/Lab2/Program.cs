6e95a5c06b4a1a4f0c02c6baf6fc0466b118c00f
vickphat <vickphat@gmail.com>
2018-11-02 20:53:48 -0400

    updated

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -10,21 +10,24 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            int number1; // 10
-            int number2; // 5
-            int product; // 50
+            int number1; //10
+            int number2; //5
+            int product; //50
 
-            Console.Write("10: "); // prompt user
+            Console.Write("10"); // prompt user
             // read first number from user
             number1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("5:"); // prompt user
+            Console.Write("5"); // prompt user
             // read second number from user
             number2 = Convert.ToInt32(Console.ReadLine());
 
-            product = 10 * 5; // 50
+            //product = 10 * 5; // 50
+            // Let's use the numbers that were input
+            product = number1 * number2
 
-            Console.WriteLine("Product is {0}, product); // display product
+
+            Console.WriteLine("Product is {0}", product); // display product
 
 
         }
