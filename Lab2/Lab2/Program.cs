e4d57d71b428a8aeffd16e789779588082b95ac0
vickphat <vickphat@gmail.com>
2018-11-03 23:10:01 -0400

    Lab 2 almost done

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -10,26 +10,45 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("{0}\n{1}", "Hello World!", "From Vicheka Phat");
+            
+            Console.Write("{0}\t{1}", "Hello World!", "From Vicheka Phat");
+
+            // A. The console application starts after you have created a new project and then typing in the nescessary code to and then running it.
+            // B. An integer type variable is a number without a decimal point. A double/floating-point variable are numbers that does have a decimal point. 
+            //Double is a double precision floating point data while float is a single precision. 
+            // C. 
+
+
+
+
+
+            Console.ReadLine();
+
+
             int number1; //10
-            int number2; //5
-            int product; //50
+                int number2; //5
+                int product; //50
 
-            Console.Write("10"); // prompt user
-            // read first number from user
-            number1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("10"); // prompt user
+                // read first number from user
+                number1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("5"); // prompt user
-            // read second number from user
-            number2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("5"); // prompt user
+                // read second number from user
+                number2 = Convert.ToInt32(Console.ReadLine());
 
-            //product = 10 * 5; // 50
-            // Let's use the numbers that were input
-            product = number1 * number2
-
-
-            Console.WriteLine("Product is {0}", product); // display product
+                //product = 10 * 5; // 50
+                // Let's use the numbers that were input
+                product = number1 * number2;
 
 
-        }
+                Console.WriteLine("Product is {0}", product); // display product
+
+
+
+
+            
+        } 
     }
 }
