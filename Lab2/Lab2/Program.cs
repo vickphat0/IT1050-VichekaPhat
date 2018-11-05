b973ea5c34fe769ca6efd7173d19e368b5a083e0
vickphat <vickphat@gmail.com>
2018-11-04 23:38:46 -0500

    Lab 2 done

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -15,9 +15,19 @@ namespace Lab2
             Console.Write("{0}\t{1}", "Hello World!", "From Vicheka Phat");
 
             // A. The console application starts after you have created a new project and then typing in the nescessary code to and then running it.
-            // B. An integer type variable is a number without a decimal point. A double/floating-point variable are numbers that does have a decimal point. 
-            //Double is a double precision floating point data while float is a single precision. 
-            // C. 
+            
+            /* B. An integer type variable is a number without a decimal point. A double/floating-point variable are numbers that does have a decimal point. 
+                  Double is a double precision floating point data while float is a single precision.*/
+           
+            /* C. An example of a method from chapter 4 is the gas pedal in a car. A method describes the mechanisms that actually perform its tasks. The method hides from its 
+                  user the complex tasks that it performs, just as the accelerator pedal of a car hides from the driver the complex mechanisms of making the car go faster.*/
+           
+            // D. To read the value of an instance variable, we create method called a accessor.  To assign a value ton instance variable, we use a method called a accessor.
+            
+            /* E. A class is a template definition of the methods and variable s in a particular kind of object. 
+                  An object is a specific instance of a class. It contains real values instead of variables 
+                  You can have as many instances as you want. However, you would want to keep it as low as you can so that the code does not get to big and confusing. */
+
 
 
 
