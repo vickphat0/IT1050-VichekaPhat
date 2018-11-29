ac0d1937774881fa03387b708b69866bfb280465
vickphat <vickphat@gmail.com>
2018-11-28 19:31:00 -0500

    started lab 6

---
+++ Lab 6/Lab 6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(months[i]);
+            }
+
+        }
+    }
+}
