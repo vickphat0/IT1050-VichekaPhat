7414c21c5f3a71883c2f5a4a3dbc4822460525e4
vickphat <vickphat@gmail.com>
2018-11-29 22:05:09 -0500

    complete

--- Lab 6/Lab 6/Program.cs
+++ Lab 6/Lab 6/Program.cs
@@ -30,19 +30,48 @@ namespace Lab_6
                 Console.WriteLine(month[i]);
             }
 
+            Console.WriteLine();
 
-
-                {
+                
                      String[] seasons = { "Fall", "Winter", "Spring", "Summer" };
 
                      foreach (string season in seasons)
                      {
                        Console.WriteLine(season);
                      }
-                }
-            
 
-            
+            Console.WriteLine();
+
+            int[] randomNumber = new int[1000];
+            Random random = new Random();
+
+            for (int i = 0; i < randomNumber.Length; i++)
+            {
+                randomNumber[i] = random.Next(0, 100);
+            }
+
+            foreach (int i in randomNumber)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.WriteLine();
+
+                    {
+                         string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+                         int i = 0;
+                         foreach (string name in names)
+                         {
+                            Console.WriteLine("{0,2}. {1}", i, names[i]);
+
+                            i++;
+                         }
+
+
+
+                    }
+
 
 
 
